9297706b24a2d2f0870e60e0200e3617b8a87e59
1227wano <1227wanho@gmail.com>
2025-07-12 17:49:39 +0900

    for test

---
+++ hankki/src/main/java/com/hankki/members/model/vo/Members.java
@@ -0,0 +1,19 @@
+package com.hankki.members.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
+public class Members {
+	
+	private 
+
+}
